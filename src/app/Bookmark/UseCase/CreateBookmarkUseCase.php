53a293714a3da00ec489a2d2f4bc7073c75e5a21
TeXmeijin <i8858garden@gmail.com>
2024-07-15 16:20:55 +0900

    feat: 🎸 to section 22

--- src/app/Bookmark/UseCase/CreateBookmarkUseCase.php
+++ src/app/Bookmark/UseCase/CreateBookmarkUseCase.php
@@ -3,7 +3,8 @@
 namespace App\Bookmark\UseCase;
 
 use App\Models\Bookmark;
-use Cardei\LinkPreview\Client;
+// use Cardei\LinkPreview\Client;
+use App\Lib\LinkPreview\LinkPreview;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
@@ -28,20 +29,17 @@ final class CreateBookmarkUseCase
      */
     public function handle(string $url, int $category, string $comment)
     {
-        // 下記のサービスでも同様のことが実現できる
-        // @see https://www.linkpreview.net/
-        $previewClient = new Client($url);
         try {
-            $preview = $previewClient->getPreview('general')->toArray();
+            $preview = (new LinkPreview())->get($url);
 
             $model = new Bookmark();
             $model->url = $url;
             $model->category_id = $category;
             $model->user_id = Auth::id();
             $model->comment = $comment;
-            $model->page_title = $preview['title'];
-            $model->page_description = $preview['description'];
-            $model->page_thumbnail_url = $preview['cover'];
+            $model->page_title = $preview->title;
+            $model->page_description = $preview->description;
+            $model->page_thumbnail_url = $preview->cover;
             $model->save();
         } catch (\Exception $e) {
             Log::error($e->getMessage());
